fc704d5adbc9bd44236f3028322043b4f7fd6072
rweathers89 <rachelb.weathers@gmail.com>
2024-06-04 20:09:07 -0700

    add api.js

---
+++ src/api.js
@@ -0,0 +1,97 @@
+// src/api.js
+
+import mockData from './mock-data';
+
+export const extractLocations = (events) => {
+    const extractedLocations = events.map((event) => event.location);
+    const locations = [...new Set(extractedLocations)];
+    return locations;
+};
+
+/**
+ *
+ * This function will fetch the list of all events
+ */
+const checkToken = async (accessToken) => {
+    const response = await fetch(
+        `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`
+    );
+    const result = await response.json();
+    return result;
+};
+
+const removeQuery = () => {
+    let newurl;
+    if (window.history.pushState && window.location.pathname) {
+        newurl =
+            window.location.protocol +
+            "//" +
+            window.location.host +
+            window.location.pathname;
+        window.history.pushState("", "", newurl);
+    } else {
+        newurl = window.location.protocol + "//" + window.location.host;
+        window.history.pushState("", "", newurl);
+    }
+};
+
+
+const getToken = async (code) => {
+    const encodeCode = encodeURIComponent(code);
+    const response = await fetch(
+        'https://4jg9bjuf80.execute-api.us-west-1.amazonaws.com/dev/api/token' + '/' + encodeCode
+    );
+    const { access_token } = await response.json();
+    access_token && localStorage.setItem("access_token", access_token);
+
+    return access_token;
+};
+
+export const getEvents = async () => {
+    if (window.location.href.startsWith('http://localhost')) {
+        return mockData;
+    }
+
+    if (!navigator.onLine) {
+        const events = localStorage.getItem("lastEvents");
+        // NProgress.done();
+        return events ? JSON.parse(events) : [];
+    }
+
+    const token = await getAccessToken();
+
+
+    if (token) {
+        removeQuery();
+        const url = 'https://eg2ouor353.execute-api.us-west-1.amazonaws.com/default/auth-server-dev-getCalendarEvents' + '/' + token;
+        //https://gzspthfo54.execute-api.us-west-1.amazonaws.com/dev/api/get-events  
+        const response = await fetch(url);
+        const result = await response.json();
+        if (result) {
+            //  NProgress.done();
+            localStorage.setItem("lastEvents", JSON.stringify(result.events));
+            return result.events;
+        } else return null;
+    }
+};
+
+export const getAccessToken = async () => {
+    const accessToken = localStorage.getItem('access_token');
+    const tokenCheck = accessToken && (await checkToken(accessToken));
+
+    if (!accessToken || tokenCheck.error) {
+        await localStorage.removeItem("access_token");
+        const searchParams = new URLSearchParams(window.location.search);
+        const code = await searchParams.get("code");
+        if (!code) {
+            const response = await fetch(
+                'https://4jg9bjuf80.execute-api.us-west-1.amazonaws.com/dev/api/get-auth-url'
+            );
+            const result = await response.json();
+            const { authUrl } = result;
+            return (window.location.href = authUrl);
+        }
+        return code && getToken(code);
+    }
+    return accessToken;
+};
